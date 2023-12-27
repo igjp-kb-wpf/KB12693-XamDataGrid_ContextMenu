50681f3ea983e8297780caf0fb71951944b93eab
mkamiishi <mkamiishi@infragistics.com>
2023-12-27 14:29:26 +0900

    Initial commit
    初回コミット

---
+++ XamDataGrid_ContextMenu/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Infragistics.Controls.Menus;
+using Infragistics.Windows;
+using Infragistics.Windows.DataPresenter;
+using Infragistics.Windows.DataPresenter.Events;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace XamDataGrid_ContextMenu
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenu_Opening(object sender, OpeningEventArgs e)
+        {
+            List<CellValuePresenter> cells = e.GetClickedElements<CellValuePresenter>();
+
+            if (cells.Count == 0 )
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                xamDataGrid1.SelectedItems.Records.Clear();
+                cells[0].Record.IsSelected = true;
+                cells[0].Record.IsActive = true;
+            }
+        }
+    }
+}
